87f4ec034e935d0f7854ea1084e0b2b62cc753d6
Brandon Whiteman <bwhiteman@gmail.com>
2012-03-15 14:12:05 +0100

    Adding oozie streaming job framework.

---
+++ asfFramework/src/main/java/com/potomacfusion/asfframework/jobs/OozieStreamingJob.java
@@ -0,0 +1,13 @@
+package com.potomacfusion.asfframework.jobs;
+
+/**
+ * User: bwhiteman
+ * Date: 3/15/12
+ * Time: 2:09 PM
+ */
+public class OozieStreamingJob implements Job {
+
+    public String getTask() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
